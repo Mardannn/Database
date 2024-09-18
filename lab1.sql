c080bf03bf4e0b3eac78e5ddec14cef462b9018c
Mardannn <m.bolatbekk.20@mail.ru>
2024-09-18 18:21:23 +0500

    Create lab1.sql

---
+++ lab1.sql
@@ -0,0 +1,21 @@
+create database lab1;
+
+create table users (
+    id serial primary key,
+    firstname varchar(50),
+    lastname varchar(50),
+    isadmin integer default 1
+);
+
+alter table users alter column isadmin drop default ;
+
+create table tasks (
+    id serial primary key,
+    name varchar(50),
+    user_id int
+);
+drop table users;
+
+drop table tasks;
+
+drop database lab1;
