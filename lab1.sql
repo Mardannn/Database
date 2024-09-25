d533d73d99950eb843d1563d3afb49c3770814e1
Mardannn <m.bolatbekk.20@mail.ru>
2024-09-25 13:50:07 +0500

    lab1.sql

--- lab1.sql
+++ lab1.sql
@@ -1,29 +1,40 @@
---1)
-create database lab1;
---2)
-create table users (
-    id serial,
-    firstname varchar(50),
-    lastname varchar(50),
-);--3)
-alter table users add column
-    isadmin integer;
---4)
-alter table users
-alter column isadmin type boolean using (isadmin::boolean);
---5)
-alter table users
-alter isadmin set default false;
+-- 1 task
+CREATE DATABASE lab1;
 
---6)
-alter table users
-add primary key(id); --7)
-create table tasks (
-    id serial primary key,
-    name varchar(50),
-    user_id int
+-- 2 task
+CREATE TABLE users (
+    id SERIAL,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50)
 );
---8)
-drop table tasks;
---9)
-drop database lab1;
+-- 3 task
+ALTER TABLE users
+ADD COLUMN isadmin INTEGER
+CHECK ( isadmin IN (1, 0) );
+
+-- 4 task
+ALTER TABLE users
+DROP CONSTRAINT IF EXISTS users_isadmin_check;
+ALTER TABLE users
+ALTER COLUMN isadmin TYPE BOOLEAN
+USING isadmin::BOOLEAN;
+
+-- 5 task
+ALTER TABLE users
+ALTER COLUMN isadmin SET  DEFAULT false;
+
+-- 6 task
+ALTER TABLE users
+ADD CONSTRAINT add_primary PRIMARY KEY (id);
+
+-- 7 task
+CREATE TABLE tasks(
+    id SERIAL,
+    name VARCHAR(50),
+    user_id INTEGER
+);
+-- 8 task
+DROP TABLE tasks;
+
+-- 9 task
+DROP DATABASE lab1;
